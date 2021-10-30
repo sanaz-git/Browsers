778ffbbc55243db641c4edc84c407ff58edb0bff
sanaz <szeraati69@gmail.com>
2021-10-30 19:06:06 +0330

    add ex3-hijacklogo

--- week1/homework/browsers/ex1-booklist/main.js
+++ week1/homework/browsers/ex1-booklist/main.js
@@ -1,60 +1,74 @@
 //it hasn't completed yet!!
 
-function bookList(){
+
   const myBooks = [
     {
       title: 'The Design of Everyday Things',
       author: 'Don Norman',
       isbn: '978-0465050659',
       alreadyRead: false,
+      imgSrc: './img/imgElement0.jpg',
     },
     {
       title: 'The Most Human Human',
       author: 'Brian Christian',
       isbn: '978-1617933431',
       alreadyRead: true,
+      imgSrc: './img/imgElement1.jpg',
     },
     {
       title: 'The Pragmatic Programmer',
       author: 'Andrew Hunt',
       isbn: '978-0201616224',
       alreadyRead: true,
+      imgSrc: './img/imgElement2.jpg',
     },
   ];
 
+  function bookList(){
   const headTitle = document.createElement('h1');
   headTitle.textContent = 'Book List'
-  // headTitle.style.fontSize = "50px";
-  // headTitle.style.fontFamily = "cursive";
-  // headTitle.style.paddingLeft = "70px";
+  headTitle.style.fontSize = "50px";
+  headTitle.style.paddingLeft = "0";
   document.body.appendChild(headTitle)
 
   const ulElement = document.createElement('ul');
-  list.style.display = "flex";
-  list.style.flexWrap = "wrap";
-  document.body.appendChild(ul);
+  ulElement.style.display = 'flex';
+  ulElement.style.flexWrap = "wrap";
+ 
+  document.body.appendChild(ulElement);
 
   for (let i=0; i < myBooks.length ; i++){
 
 
     const liElement = document.createElement('li');
     liElement.style.listStyle = "none";
-    liElement.style.padding = "25px";
-    liElement.style.margin = "25px";
-    liElement.style.width = "400px";
+    // liElement.style.padding = "25px";
+    // liElement.style.margin = "25px";
+    liElement.style.width = "300px";
     liElement.style.borderRadius = "25px"
     ulElement.appendChild(liElement);
 
     const pElement = document.createElement('p');
     pElement.textContent = `${myBooks[i].title} - ${myBooks[i].author}`;
-    liElement.appendChild(pElement);
+    liElement.style.width = "300px";
+    // pElement.style.margin ="5px";
+    ulElement.appendChild(pElement);
+
+    const imgElement = document.createElement("img");
+    imgElement.src = myBooks[i].imgSrc;
+    imgElement.setAttribute('alt', myBooks[i].title);
+    imgElement.style.display = 'block';
+    // imgElement.style.height = '400px';
+    imgElement.style.marginLeft = 'auto';
+    imgElement.style.marginRight = 'auto';
+    imgElement.style.marginBottom = '20px';
+    imgElement.style.width = '70%';
+    liElement.appendChild(imgElement);
+
 
    
-    const imgElement = document.createElement('img');
-    imgElement[i] = new Image();
-    imgElement[i].src = 'imgElement.png' ;
-    imgElement[i].style.paddingTop = "20px";
-    liElement.appendChild(imgElement[i]);  
+    
 
   }
 }
---
+++ week1/homework/browsers/ex3_hijacklogo.js
@@ -0,0 +1,22 @@
+// In this exercise you're expected to write a JavaScript function that can be executed in the console of the Google website.
+// Find out how to select the element that contains the Google logo, and store it in a variable.
+// Modify the src and srcset of the logo so that it's replaced by the HackYourFuture logo instead.
+
+
+
+
+// const googleLogo = document.getElementById("hplogo")
+// googleLogo.addEventListener('click', myFunction())
+
+// function myFunction() {
+//     document.getElementById("logo").src = "https://www.hackyourfuture.dk/static/logo-dark.svg";
+//   }
+
+
+function hijackGoogleLogo() {
+    const googleLogo = document.getElementById('hplogo');
+    googleLogo.src = 'https://www.hackyourfuture.dk/static/logo-dark.svg';
+    googleLogo.srcset = 'https://www.hackyourfuture.dk/static/logo-dark.svg';
+  }
+  
+  hijackGoogleLogo();
