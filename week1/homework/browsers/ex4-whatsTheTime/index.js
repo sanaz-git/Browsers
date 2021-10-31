fee855d394b55b09867db54770f36365181d36a5
sanaz <szeraati69@gmail.com>
2021-10-31 18:35:29 +0330

    add another method for exercise 4

--- week1/homework/browsers/ex4-whatsTheTime/index.js
+++ week1/homework/browsers/ex4-whatsTheTime/index.js
@@ -1,52 +1,47 @@
-// function addCurrentTime(){
-//     let currentDate = new Date(); 
-//     let dateTime = "Last Sync: " + currentDate.getDate() + "/"
-//                     + (currentDate.getMonth()+1)  + "/" 
-//                     + currentDate.getFullYear() + " @ "  
-//                     + currentDate.getHours() + ":"  
-//                     + currentDate.getMinutes() + ":" 
-//                     + currentDate.getSeconds();
-// }
-
-// setInterval(addCurrentTime);
-
-
-
-// function addCurrentTime(){
-//     var d = new Date();
-//     var s = d.getSeconds();
-//     var m = d.getMinutes();
-//     var h = d.getHours();
-//     document.write(h + ":" + m + ":" + s);
-// }
-
-// setInterval(addCurrentTime,1000);
-function showTime(){
-    var date = new Date();
-    var h = date.getHours(); 
-    var m = date.getMinutes(); 
-    var s = date.getSeconds(); 
-    var session = "AM";
-    
-    if(h == 0){
-        h = 12;
+//first method
+function main() {
+  
+    function clock() {
+      const currentTime = new Date();
+      document.body.textContent = currentTime.toLocaleTimeString();
+      
+      console.log(currentTime);
     }
+  
+    window.setInterval(clock, 1000);
+  
+  }
+  
+  main()
+
+
+// second method
+// function showTime(){
+//     var date = new Date();
+//     var h = date.getHours(); 
+//     var m = date.getMinutes(); 
+//     var s = date.getSeconds(); 
+//     var session = "AM";
     
-    if(h > 12){
-        h = h - 12;
-        session = "PM";
-    }
+//     if(h == 0){
+//         h = 12;
+//     }
     
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
+//     if(h > 12){
+//         h = h - 12;
+//         session = "PM";
+//     }
     
-    var time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("DigitalCLOCK").innerText = time;
-    document.getElementById("DigitalCLOCK").textContent = time;
+//     h = (h < 10) ? "0" + h : h;
+//     m = (m < 10) ? "0" + m : m;
+//     s = (s < 10) ? "0" + s : s;
     
-    setTimeout(showTime, 1000);
+//     var time = h + ":" + m + ":" + s + " " + session;
+//     document.getElementById("DigitalCLOCK").innerText = time;
+//     document.getElementById("DigitalCLOCK").textContent = time;
     
-}
+//     setTimeout(showTime, 1000);
+    
+// }
  
-showTime();
+// showTime();
