5d5027369634f5c81de0205c3eea0c1fcb16da3d
sanaz <szeraati69@gmail.com>
2021-11-03 19:09:51 +0330

    finish cat walk

--- week1/homework/browsers/ex5-catWalk/index.js
+++ week1/homework/browsers/ex5-catWalk/index.js
@@ -1,7 +1,34 @@
-let imgElement= document.getElementsByTagName('img')[0];
+const imgElement = document.querySelector('body img');
 imgElement.style.Left = '0px';
+let pos = 0;
 
-function catWalk(){
-    imgElement.style.left= (parseInt(imgElement.style.left) + 10) + 'px';
+function main(){
+    const timeCatWalk =setInterval(catWalk, 50);
+
+    function catWalk(){  
+        if (pos > screen.width){
+            pos =0;
+        } else if(pos == (screen.width/2)-150){
+            clearInterval(timeCatWalk)
+            imgElement.src= 'tenor.gif'
+            setTimeout(() => {imgElement.src ='cat-walk.gif';
+            pos += 10;
+            main();
+        },5000)
+        }else{
+            imgElement.style.left = `${pos}px`;
+            pos += 10;
+        }
+     }
+   
+    
+
 }
-window.setInterval(catWalk, 50);
+
+main() ;
+
+
+
+
+
+
