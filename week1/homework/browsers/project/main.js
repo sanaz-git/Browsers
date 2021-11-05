911b38faf588b5cdf86a430db7cdc5228513eabe
sanaz <szeraati69@gmail.com>
2021-11-05 19:28:31 +0330

    add main.js file

--- week1/homework/browsers/project/main.js
+++ week1/homework/browsers/project/main.js
@@ -1,11 +1,34 @@
-const qoute = [];
-const author =[];
-
-function quote() {
-
-    const randomGenerator = qoute[Math.floor(Math.random() * qoute.length)] ;
+const qoutes = [
+    {
+      quote: "Change the world by being yourself.",
+      author:"Amy Poehler"
+    },
+    {
+      quote: "Whatever you do, do it well. ",
+      author:" Walt Disney"
+    },
+    {
+      quote: "Everything you can imagine is real.",
+      author:"Pablo Picasso"
+    },
+    {
+      quote: "Dream as if you’ll live forever, live as if you’ll die today.",
+      author:"James Dean"
+    },
+    {
+      quote: "You do not find the happy life. You make it.",
+      author:"Camilla Eyring Kimball"
+    }
+];
     
     
+    
+const icon = document.getElementById('button');
+const p = document.querySelector('p');
+const h3 = document.querySelector('h3');
 
-  
-}
+button.addEventListener('click', function(){
+    let randomQuotes = quotes[Math.floor(Math.random() * qoutes.length)] 
+    p.textContent = quotes[randomQuotes].quote;
+    h3.textContent = quotes[randomQuotes].author;
+});
