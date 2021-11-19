c125bf83c6ae734859065cbcc6e22428aadb8248
sanaz <szeraati69@gmail.com>
2021-11-19 20:12:28 +0330

    fix bug

--- week2/browser/homework/rock-paper-scissors-game/main.js
+++ week2/browser/homework/rock-paper-scissors-game/main.js
@@ -1,16 +1,16 @@
-const choices = document.querySelectorAll('.choice');
-const score = document.getElementById('score');
-const result = document.getElementById('result');
-const restart = document.getElementById('restart');
-const modal = document.querySelector('.modal');
+function main(){const choices = document.querySelectorAll(".choice");
+const score = document.getElementById("score");
+const result = document.getElementById("result");
+const restart = document.getElementById("restart");
+const modal = document.querySelector(".modal");
 const scoreboard = {
   player: 0,
-  computer: 0
+  computer: 0,
 };
 
 // Play game
 function play(e) {
-  restart.style.display = 'inline-block';
+  restart.style.display = "inline-block";
   const playerChoice = e.target.id;
   const computerChoice = getComputerChoice();
   const winner = getWinner(playerChoice, computerChoice);
@@ -21,66 +21,69 @@ function play(e) {
 function getComputerChoice() {
   const rand = Math.random();
   if (rand < 0.34) {
-    return 'rock';
+    return "rock";
   } else if (rand <= 0.67) {
-    return 'paper';
+    return "paper";
   } else {
-    return 'scissors';
+    return "scissors";
   }
 }
 
 // Get game winner
 function getWinner(p, c) {
   if (p === c) {
-    return 'draw';
-  } else if (p === 'rock') {
-    if (c === 'paper') {
-      return 'computer';
+    return "draw";
+  } else if (p === "rock") {
+    if (c === "paper") {
+      return "computer";
     } else {
-      return 'player';
+      return "player";
     }
-  } else if (p === 'paper') {
-    if (c === 'scissors') {
-      return 'computer';
+  } else if (p === "paper") {
+    if (c === "scissors") {
+      return "computer";
     } else {
-      return 'player';
+      return "player";
     }
-  } else if (p === 'scissors') {
-    if (c === 'rock') {
-      return 'computer';
+  } else if (p === "scissors") {
+    if (c === "rock") {
+      return "computer";
     } else {
-      return 'player';
+      return "player";
     }
   }
 }
 
 function showWinner(winner, computerChoice) {
-  if (winner === 'player') {
+  if (winner === "player") {
     // Inc player score
     scoreboard.player++;
     // Show modal result
     result.innerHTML = `
       <h1 class="text-win">You Win</h1>
       <i class="fas fa-hand-${computerChoice} fa-10x"></i>
-      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() +
-        computerChoice.slice(1)}</strong></p>
+      <p>Computer Chose <strong>${
+        computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+      }</strong></p>
     `;
-  } else if (winner === 'computer') {
+  } else if (winner === "computer") {
     // Inc computer score
     scoreboard.computer++;
     // Show modal result
     result.innerHTML = `
       <h1 class="text-lose">You Lose</h1>
       <i class="fas fa-hand-${computerChoice} fa-10x"></i>
-      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() +
-        computerChoice.slice(1)}</strong></p>
+      <p>Computer Chose <strong>${
+        computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+      }</strong></p>
     `;
   } else {
     result.innerHTML = `
       <h1>It's A Draw</h1>
       <i class="fas fa-hand-${computerChoice} fa-10x"></i>
-      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() +
-        computerChoice.slice(1)}</strong></p>
+      <p>Computer Chose <strong>${
+        computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+      }</strong></p>
     `;
   }
   // Show score
@@ -89,7 +92,7 @@ function showWinner(winner, computerChoice) {
     <p>Computer: ${scoreboard.computer}</p>
     `;
 
-  modal.style.display = 'block';
+  modal.style.display = "block";
 }
 
 // Restart game
@@ -105,11 +108,14 @@ function restartGame() {
 // Clear modal
 function clearModal(e) {
   if (e.target == modal) {
-    modal.style.display = 'none';
+    modal.style.display = "none";
   }
 }
 
 // Event listeners
-choices.forEach(choice => choice.addEventListener('click', play));
-window.addEventListener('click', clearModal);
-restart.addEventListener('click', restartGame);
+choices.forEach((choice) => choice.addEventListener("click", play));
+window.addEventListener("click", clearModal);
+restart.addEventListener("click", restartGame);
+}
+
+main();
