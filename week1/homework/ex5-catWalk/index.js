7b0edd94e9afac38b7edd9ca9375d3477e751201
Sanaz <szeraati69@gmail.com>
2022-05-16 20:28:03 +0430

    edit files

---
+++ week1/homework/ex5-catWalk/index.js
@@ -0,0 +1,31 @@
+const imgElement = document.querySelector('body img');
+imgElement.style.Left = '0px';
+let pos = 0;
+
+function main(){
+    const timeCatWalk =setInterval(catWalk, 50);
+
+    function catWalk(){  
+        if (pos > screen.width){
+            pos =0;
+        } else if(pos == (screen.width/2)-150){
+            clearInterval(timeCatWalk)
+            imgElement.src= 'tenor.gif'
+            setTimeout(() => {imgElement.src ='cat-walk.gif';
+            pos += 10;
+            main();
+        },5000)
+        }else{
+            imgElement.style.left = `${pos}px`;
+            pos += 10;
+        }
+     }
+}
+
+main() ;
+
+
+
+
+
+
